cab85b75ff0cd3bcdc5bdf12e6bb694bb175df1f
efraim urel <efraimurelpalodang@gmail.com>
2024-11-21 07:46:07 +0800

    membuat harga menggunakan framework alpineJS dan membuat konversi mata uang ke rupiah

--- src/app.js
+++ src/app.js
@@ -1,8 +1,17 @@
 document.addEventListener('alpine:init', () => {
   Alpine.data('products', () => ({
     items: [
-      {id: 1, name: 'Coffe Beans One', img: '2.jpg', price: 30000},
-      {id: 1, name: 'Coffe Beans One', img: 'produk.jpg', price: 45000}
+      {id: 1, name: 'Coffe Beans One', img: '2.jpg', price: 30000, pawal: 70000},
+      {id: 1, name: 'Arabica Blend', img: '1.jpg', price: 45000, pawal: 100000}
     ]
   }))
 })
+
+// Konversi mata uang ke rupiah
+const rupiah = (number) => {
+  return new Intl.NumberFormat('id-ID', {
+    style: 'currency',
+    currency: 'IDR',
+    minimumFractionDigits: 0
+  }).format(number);
+};
