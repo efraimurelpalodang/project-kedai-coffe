242e62037cf3d02e3238f80938cc6356a5701282
efraim urel <efraimurelpalodang@gmail.com>
2024-11-21 08:13:27 +0800

    membuat notifikasi di keranjangnya jika ditambahkan

--- src/app.js
+++ src/app.js
@@ -4,7 +4,19 @@ document.addEventListener('alpine:init', () => {
       {id: 1, name: 'Coffe Beans One', img: '2.jpg', price: 30000, pawal: 70000},
       {id: 1, name: 'Arabica Blend', img: '1.jpg', price: 45000, pawal: 100000}
     ]
-  }))
+  }));
+
+  Alpine.store('cart', {
+    items: [],
+    total: 0,
+    quantity: 0,
+    add(newItem) {
+      this.items.push(newItem);
+      this.quantity++;
+      this.total += newItem.price;
+      console.log(this.total);
+    }
+  });
 })
 
 // Konversi mata uang ke rupiah
